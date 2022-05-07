675482f25f02e0fc905491993b5dd5d0f116ee23
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-08 02:20:17 +0530

    [Aniket]Adding UC5

---
+++ UC5_Exact100position.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class UC5_Exact100position
+    {
+        public static void UC5()
+        {
+            //local variable
+            int rollDice, playerPos = 0;
+            int count = 0;
+
+            //To generate random number
+            Random random = new Random();
+            //List to check options
+            var list = new List<string> { "No Play", "Ladder", "Snake" };
+
+            //Conditions for user options
+            rollDice = random.Next(1, 7);
+            Console.WriteLine("Dice number is : " + rollDice);
+
+            //Print user options
+            Console.WriteLine("Player checks options 1.No Play  2.Ladder   3.Snake");
+
+            //Use random to check user option
+            int pos = random.Next(list.Count);
+            Console.WriteLine("Player option is : " + list[pos]);
+            if (playerPos + rollDice < 100)
+            {
+                if (list[pos] == "Ladder")
+                {
+                    playerPos += rollDice;
+                }
+                if (list[pos] == "Snake")
+                {
+                    playerPos -= rollDice;
+                }
+            }
+            else if (playerPos + rollDice == 100 && list[pos] == "Ladder")
+            {
+                playerPos += rollDice;
+
+            }
+
+            if (playerPos < 0)
+            {
+                playerPos = 0;
+            }
+            //Print to Console
+            Console.WriteLine("\n");
+            Console.WriteLine("Player current position : " + playerPos);
+            count++;
+            Console.WriteLine("Final position is :" + playerPos);
+            Console.WriteLine("Total MOves:" + count);
+        }
+    }
+}
